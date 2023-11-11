e944563f53fdba5faa2a9adf6fbf540a237c7724
lbovio <omicronvt@gmail.com>
2023-11-11 12:44:29 -0300

    Homework M8

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,15 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   res =  nombre.charAt(0).toUpperCase() + nombre.slice(1);
+   return res
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +21,8 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   res = cb(num1, num2)
+   return res
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +31,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   sum = 0
+   for (i = 0; i<arrayOfNumbers.length; i++){
+      sum = sum+arrayOfNumbers[i]
+   }   
+   cb(sum)
 }
 
 function forEach(array, cb) {
@@ -33,6 +43,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   for (i = 0; i<array.length; i++){
+      cb(array[i])
+   }
 }
 
 function map(array, cb) {
@@ -40,12 +53,28 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   res = []
+   for (i = 0; i<array.length; i++){
+      res.push(cb(array[i]))
+   }
+   return res
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   res = []
+   a = arrayOfStrings
+   for (i = 0; i<a.length; i++){
+      if (a[i].charAt(0)==='a'){
+         res.push(a[i])
+      }
+      
+   }
+   return res
+
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
